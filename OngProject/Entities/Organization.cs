7c129ea392f72d52178e29d9dd5df8547ccd3166
AngelVinet <101446482+AngelVinet@users.noreply.github.com>
2022-07-14 19:37:59 -0400

    Change Name class

--- OngProject/Entities/Organization.cs
+++ OngProject/Entities/Organization.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OngProject.Entities
 {
-    public class Organizacion : Entity
+    public class Organization : Entity
     {
         [MaxLength(255)]
         [Required]
