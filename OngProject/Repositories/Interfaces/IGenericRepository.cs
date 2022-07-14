ca4d729983f5cb9f9900ec94c672e83f9ca3e714
Lucas <100593063+LucasfVarela@users.noreply.github.com>
2022-07-14 21:01:40 +0100

    Feature/GenericRepo

---
+++ OngProject/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿
+using OngProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+namespace OngProject.Repositories.Interfaces
+{
+    public interface IGenericRepository<T> where T : Entity
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<IEnumerable<T>> find(Expression<Func<T, bool>> predicate);
+        Task Insert(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+        Task<T> GetById(int? id);
+    }
+}
