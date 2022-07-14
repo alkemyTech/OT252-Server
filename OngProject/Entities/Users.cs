4c8f72bd5bb7cc03aede6618f83f8476e1feb1df
AngelVinet <101446482+AngelVinet@users.noreply.github.com>
2022-07-14 19:58:54 -0400

    Add class Users

---
+++ OngProject/Entities/Users.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Entities
+{
+    public class Users  : Entity
+    {
+        [MaxLength(255)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string LastName { get; set; }
+
+        [MaxLength(320)]
+        [Required]
+        public string Email { get; set; }
+
+        [MaxLength(20)]
+        [Required]
+        public string Password { get; set; }
+
+        [MaxLength(255)]
+        public string Photo { get; set; }
+
+        public int RoleId { get; set; }
+        [ForeignKey("RoleId")]
+        public Role Role { get; set; }
+    }
+}
