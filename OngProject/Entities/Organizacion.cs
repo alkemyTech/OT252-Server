82f18e49e7352e3bbd9a27b2561e8e4fab8d916a
AngelVinet <101446482+AngelVinet@users.noreply.github.com>
2022-07-14 19:32:27 -0400

    Add class Organization

---
+++ OngProject/Entities/Organizacion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Entities
+{
+    public class Organizacion : Entity
+    {
+        [MaxLength(255)]
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string Image { get; set; }
+
+        public string Address { get; set; }
+
+        public ulong Phone { get; set; }
+
+        [MaxLength(320)]
+        [Required]
+        public string Email { get; set; }
+
+        [MaxLength(500)]
+        [Required]
+        public char[] WelcomeText { get; set; }
+
+        [MaxLength(2000)]
+        public char[] AboutUsText { get; set; }
+    }
+}
