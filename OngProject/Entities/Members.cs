a70b5094f321c3286305e06c218961b335757eff
Emilio Merchan <emimerchan83@gmail.com>
2022-07-15 11:13:28 -0300

    Added Entity Members

---
+++ OngProject/Entities/Members.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Entities
+{
+    public class Members : Entity
+    {
+        [MaxLength(255)]
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string FacebookUrl { get; set; }
+       
+        [MaxLength(255)]
+        public string InstragramUrl { get; set; }
+        
+        [MaxLength(255)]
+        public string LinkedinUrl { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string Image { get; set; }
+    }
+}
