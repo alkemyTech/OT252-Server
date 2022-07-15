23388ed971a74a53a4933c30a496e28fe79a3533
Lucas <100593063+LucasfVarela@users.noreply.github.com>
2022-07-15 18:20:24 +0100

    feature/AddControllerRole

---
+++ OngProject/Controllers/RoleController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OngProject.Core.Interfaces;
+
+namespace OngProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IRoleService roleService;
+
+        public RoleController(IUnitOfWork unitOfWork, IRoleService roleService)
+        {
+            this.unitOfWork = unitOfWork;
+            this.roleService = roleService;
+        }
+    }
+}
