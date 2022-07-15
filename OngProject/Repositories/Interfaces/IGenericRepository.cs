7c0a568830b5522f83a0a9b85a65db11fd847d87
jraimondo <jraimondo@correo.dnic.gub.uy>
2022-07-15 10:52:40 -0300

    Se elimina un new()

--- OngProject/Repositories/Interfaces/IGenericRepository.cs
+++ OngProject/Repositories/Interfaces/IGenericRepository.cs
@@ -6,7 +6,7 @@ using System.Linq.Expressions;
 using System.Threading.Tasks;
 namespace OngProject.Repositories.Interfaces
 {
-    public interface IGenericRepository<T> where T : Entity, new()
+    public interface IGenericRepository<T> where T : Entity
     {
         Task<IEnumerable<T>> GetAll();
         Task<IEnumerable<T>> Find(Expression<Func<T, bool>> predicate);
