d418a6d81295609614b0d7677afbb5b3f91dcbbb
Emiliano ZERBINO <emilianozerbino@yahoo.com.ar>
2022-07-14 21:56:41 -0300

    Agregada la Entidad Slide

---
+++ OngProject/Entities/Slide.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OngProject.Entities
+{
+    public class Slide : Entity
+    {
+        [MaxLength(255)]
+        [Required]
+        public String ImageUrl { get; set; }
+
+        [MaxLength(500)]
+        [Required]
+        public String Text { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public String Order { get; set; }  //¡¡¡Chequear que sea de tipo String!!!, no se de que se trata la variable Order
+
+        [ForeignKey("Organization")]
+        [Required]
+        public int OrganizationId { get; set; }
+        public Organization Organization { get; set; }
+    }
+}
