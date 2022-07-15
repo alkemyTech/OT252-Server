c05458de75c2fb0e29f3eff80e784bb3aa0d98d5
AngelVinet <101446482+AngelVinet@users.noreply.github.com>
2022-07-14 20:09:55 -0400

    Add class Role

---
+++ OngProject/Entities/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Entities
+{
+    public class Role : Entity
+    {
+        [MaxLength(255)]
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Description { get; set; }
+    }
+}
