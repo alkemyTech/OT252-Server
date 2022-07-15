ed37bfedf57390c855f72a45e7bf42e04556974d
Rafael Moragues <moraguesrafael@gmail.com>
2022-07-14 22:51:30 -0300

    modelo comment

---
+++ OngProject/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class Comment : Entity
+    {
+        [Required]
+        public int User_Id { get; set; }
+        [MaxLength(2000)]
+        [Required]
+        public string Body { get; set; }
+        [Required]
+        public int News_Id { get; set; }
+        public News? News_id { get; set; }
+    }
+}
