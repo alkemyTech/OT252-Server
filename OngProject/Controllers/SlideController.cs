06878234f8fd57ff714a9d72149eb93d181d833e
Emiliano ZERBINO <emilianozerbino@yahoo.com.ar>
2022-07-17 11:55:42 -0300

    Agregue repositorio, controlador y servicios de Slide

--- OngProject/Controllers/SlideController.cs
+++ OngProject/Controllers/SlideController.cs
@@ -1,11 +1,105 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using OngProject.Core.Business;
+using OngProject.Core.Interfaces;
+using OngProject.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OngProject.Controllers
 {
-    public class SlideController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SlideController : ControllerBase
     {
+
+        private readonly ISlideService slideService;
+
+        public SlideController(SlideService slideService)
+        {
+            this.slideService = slideService;
+        }
+
+        [Route("GetAll")]
+        [HttpGet]
+
+        public ActionResult<IEnumerable<Slide>> GetAll()
+        {
+            try
+            {
+                var slideList = slideService.GetAll();
+                return Ok(slideList);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Slide> Get(int id)
+        {
+            try
+            {
+                var slide = slideService.GetById(id);
+                return Ok(slide);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<Slide> Post([FromBody] Slide slide)
+        {
+            try
+            {
+                var newSlide = slideService.Insert(slide);
+
+                return Ok(newSlide);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpPut]
+        public ActionResult<Slide> Put([FromBody] Slide slide)
+        {
+            try
+            {
+                var editSlide = slideService.Update(slide);
+
+                return Ok(editSlide);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<bool> Delete(int id)
+        {
+            try
+            {
+                var deleteSlide = slideService.Delete(id);
+
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+        }
     }
 }
---
+++ OngProject/Core/Interfaces/ISlideService.cs
@@ -0,0 +1,22 @@
+﻿using OngProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Interfaces
+{
+    public interface ISlideService
+    {
+        IEnumerable<Slide> GetAll();
+
+        Slide GetById(int? id);
+
+        IEnumerable<Slide> Find(Expression<Func<Slide, bool>> predicate);
+
+        Slide Insert(Slide slide);
+        Slide Update(Slide slide);
+        bool Delete(int id);
+    }
+}
