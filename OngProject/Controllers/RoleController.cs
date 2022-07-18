3e37540f9eb6608db345f03b0a75f01241d8363c
Lucas <100593063+LucasfVarela@users.noreply.github.com>
2022-07-18 20:59:09 +0100

    feature/Role

---
+++ OngProject/Controllers/RoleController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OngProject.Core.Interfaces;
+using OngProject.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace OngProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRoleService roleService;
+
+        public RoleController(IRoleService roleService)
+        {
+            this.roleService = roleService;
+        }
+
+        [Route("GetAll")]
+        [HttpGet]
+        public ActionResult<IEnumerable<Role>> GetAll()
+        {
+
+            try
+            {
+                var newsList = roleService.GetAll();
+
+                return Ok(newsList);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<Role> Get(int id)
+        {
+            try
+            {
+                var news = roleService.GetById(id);
+
+                return Ok(news);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+
+        [HttpPost]
+        public ActionResult<Role> Post([FromBody] Role role)
+        {
+            try
+            {
+                var newRole = roleService.Insert(role);
+
+                return Ok(newRole);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+
+        [HttpPut]
+        public ActionResult<Role> Put([FromBody] Testimony role)
+        {
+            try
+            {
+                var editRole = roleService.Update(role);
+
+                return Ok(editRole);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+
+        [HttpDelete("{id}")]
+        public ActionResult<bool> Delete(int id)
+        {
+            try
+            {
+                var deleteRole = roleService.Delete(id);
+
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
+}
---
+++ OngProject/Core/Interfaces/IRoleService.cs
@@ -0,0 +1,20 @@
+﻿using OngProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+namespace OngProject.Core.Interfaces
+{
+    public interface IRoleService
+    {
+        IEnumerable<Role> GetAll();
+
+        Role GetById(int? id);
+
+        IEnumerable<Role> Find(Expression<Func<Role, bool>> predicate);
+
+        Role Insert(Role role);
+        Role Update(Role role);
+        bool Delete(int id);
+    }
+}
