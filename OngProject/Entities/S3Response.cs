9a2527f486e57abdda5d85abbbe79e7a6bc50076
Lucas <100593063+LucasfVarela@users.noreply.github.com>
2022-07-19 16:18:53 +0100

    feature/US-45

---
+++ OngProject/Entities/S3Response.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace OngProject.Entities
+{
+    public class S3Response
+    {
+        public HttpStatusCode Status { get; set; }
+        public string Message { get; set; }
+    }
+}
