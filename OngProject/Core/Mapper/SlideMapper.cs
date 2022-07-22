14b73975b05e4a30f08c25986884b5c037ca9883
Emilio Merchan <emimerchan83@gmail.com>
2022-07-22 20:06:42 -0300

    Added DtoSlide, Added MapperSlide

---
+++ OngProject/Core/Mapper/SlideMapper.cs
@@ -0,0 +1,27 @@
+﻿using OngProject.Core.Models.DTOs;
+using OngProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Mapper
+{
+    public class SlideMapper
+    {
+        public IEnumerable<SlideDto> ConvertListToDto(IEnumerable<Slide> listSlides)
+        {
+            List<SlideDto> listDtos = new List<SlideDto>();
+
+            foreach (Slide slide in listSlides)
+            {
+                SlideDto slideDto = new SlideDto();
+                slideDto.UrlImage = slide.ImageUrl;
+                slideDto.Text = slide.Text;
+                slideDto.Order = slide.Order;
+                listDtos.Add(slideDto);
+            }
+            return listDtos;
+        }
+    }
+}
