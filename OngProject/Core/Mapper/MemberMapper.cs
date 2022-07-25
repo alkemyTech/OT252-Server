3bdc767b17e40fe9448e4eef881b42d098a18616
Pablo <pluna.e@hotmail.com>
2022-07-25 18:04:16 -0300

    create MemberMapper

---
+++ OngProject/Core/Mapper/MemberMapper.cs
@@ -0,0 +1,38 @@
+﻿using OngProject.Core.Models.DTOs;
+using OngProject.Entities;
+using System.Collections.Generic;
+
+namespace OngProject.Core.Mapper
+{
+    public class MemberMapper
+    {
+        public IEnumerable<MemberDto> ConvertListToDto(IEnumerable<Member> listMembers)
+        {
+            List<MemberDto> listDtos = new List<MemberDto>();
+
+            foreach (Member member in listMembers)
+            {
+                MemberDto memberDto = new MemberDto();
+                memberDto.Name = member.Name;
+                memberDto.FacebookUrl = member.FacebookUrl;
+                memberDto.InstagramUrl = member.InstragramUrl;
+                memberDto.LinkedinUrl = member.LinkedinUrl;
+                memberDto.Image = member.Image;
+                listDtos.Add(memberDto);
+            }
+            return listDtos;
+        }
+
+        public MemberDto ConvertToDto (Member member)
+        {
+            MemberDto memberDto = new MemberDto();
+            memberDto.Name = member.Name;
+            memberDto.FacebookUrl = member.FacebookUrl;
+            memberDto.InstagramUrl = member.InstragramUrl;
+            memberDto.LinkedinUrl = member.LinkedinUrl;
+            memberDto.Image = member.Image;
+
+            return memberDto;
+        }
+    }
+}
