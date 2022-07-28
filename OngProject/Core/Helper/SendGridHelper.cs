355facd1de9ab7e825cf4bd8d580102ef0a2ffe7
Emilio Merchan <emimerchan83@gmail.com>
2022-07-27 23:45:35 -0300

    Added Message with template

--- OngProject/Core/Helper/SendGridHelper.cs
+++ OngProject/Core/Helper/SendGridHelper.cs
@@ -4,6 +4,7 @@ using SendGrid;
 using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,17 +18,31 @@ namespace OngProject.Core.Helper
             _config = config;
         }
 
-        public async Task WelcomeEmail(string email, string _subject, string content)
+        public async Task WelcomeEmail(string email)
         {
+            var subject = "Bienvenido";
+            var content = "Este es un mensaje de bienvenida";
             var apiKey = _config["ApiSendGrid"];
+            var template = GetTemplate(subject, content);
             var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("alkemygrupo.252@gmail.com", "Usuario de Ejemplo");
-            var subject = _subject;
+            var from = new EmailAddress("alkemygrupo.252@gmail.com", "Grupo 252 C#");
             var to = new EmailAddress(email, email);
-            var plainTextContent = content;
-            var htmlContent = "<strong>mensaje de prueba</strong>";
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", template);
             var response = await client.SendEmailAsync(msg);
+        }
+
+        public static string GetTemplate(string title, string content)
+        {
+            var outPutDirectory = Path.GetDirectoryName(Directory.GetCurrentDirectory());
+            var FilePath = Path.Combine(outPutDirectory, "OngProject\\Templates\\emailtemplate.html");
+            using (StreamReader sr = File.OpenText(FilePath))
+            {
+                string s = sr.ReadToEnd();
+                s = s.Replace("Titulo", title);
+                s = s.Replace("Texto del email", content);
+                return s;
+            }
+       
         }
     }
 }
