88b356843d0ecb90d542c57b40676917922a4b6f
Pablo <pluna.e@hotmail.com>
2022-07-29 16:10:53 -0300

    added activityMapper-ActivityDto

---
+++ OngProject/Core/Mapper/ActivityMapper.cs
@@ -0,0 +1,45 @@
+﻿using OngProject.Core.Models.DTOs;
+using OngProject.Entities;
+using System.Collections.Generic;
+
+namespace OngProject.Core.Mapper
+{
+    public class ActivityMapper
+    {
+        public IEnumerable<ActivityDto> ConvertListToDto(IEnumerable<Activity> listActivity)
+        {
+            List<ActivityDto> listDtos = new List<ActivityDto>();
+
+            foreach (Activity activity in listActivity)
+            {
+                ActivityDto activityDto = new ActivityDto();
+                activityDto.Name = activity.Name;
+                activityDto.Content = activity.Content;
+                activityDto.Image = activity.Image;
+
+                listDtos.Add(activityDto);
+            }
+            return listDtos;
+        }
+
+        public ActivityDto ConvertToDto(Activity activity)
+        {
+            ActivityDto activityDto = new ActivityDto();
+            activityDto.Name = activity.Name;
+            activityDto.Content = activity.Content;
+            activityDto.Image = activity.Image;
+
+            return activityDto;
+        }
+
+        public Activity ConvertToActivity(ActivityDto activityDto)
+        {
+            Activity activity = new Activity();
+            activity.Name = activityDto.Name;
+            activity.Content = activityDto.Content;
+            activity.Image = activityDto.Image;
+
+            return activity;
+        }
+    }
+}
---
+++ OngProject/Core/Models/DTOs/ActivityDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Core.Models.DTOs
+{
+    public class ActivityDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+        public string Image { get; set; }
+    }
+}
