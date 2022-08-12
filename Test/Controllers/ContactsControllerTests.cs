9387007198e8be1752f64303657c0aa68a26c546
jraimondo <jraimondo@correo.dnic.gub.uy>
2022-08-12 09:57:14 -0300

    Creacion de ContactControllerTest

---
+++ Test/Controllers/ContactsControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OngProject.Core.Interfaces;
+
+namespace Test.Controllers
+{
+    [TestClass]
+    public class ContactsControllerTests 
+    {
+
+        public IContactService _contactService { get; set; }
+
+
+        
+
+        
+
+
+    }
+}
