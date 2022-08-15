961dffae8a57e3b8a870d333f0512e23cd40a685
jraimondo <jraimondo@correo.dnic.gub.uy>
2022-08-15 10:38:56 -0300

    no se avanza

--- Test/Controllers/ContactsControllerTests.cs
+++ Test/Controllers/ContactsControllerTests.cs
@@ -1,19 +1,55 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using OngProject.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OngProject.Core.Interfaces;
+
+namespace OngProject.Controllers.Tests
+{
+    [TestClass()]
+    public class ContactsControllerTests
+    {
+        [TestMethod()]
+        public void ContactsControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PostTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PutTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
 
 namespace Test.Controllers
 {
     [TestClass]
     public class ContactsControllerTests 
     {
-
-        public IContactService _contactService { get; set; }
-
-
-        
-
-        
-
 
     }
 }
