7fa76e13d33dbed5c84b6f8e3bc5587e573a94ac
AngelVinet <101446482+AngelVinet@users.noreply.github.com>
2022-08-17 04:05:08 -0400

    Middleware role autorization Add the file RoleAutorizationMiddleware.cs Modified the file Startup.cs Fix appsettings.json

---
+++ OngProject/Middleware/RoleAutorizationMiddleware.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace OngProject.Middleware
+{
+    public class RoleAutorizationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RoleAutorizationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            bool containsPath = false;
+            var method = context.Request.Method;
+            var path = context.Request.Path.ToString();
+            var role = context.User.Claims.FirstOrDefault(u => u.Type == ClaimTypes.Role);
+
+            List<string> methods = new()
+            {
+                "PATCH",
+                "PUT",
+                "POST",
+                "DELETE"
+            };
+            List<string> paths = new List<string>();
+            paths.Add("/activity");
+            paths.Add("/activities");
+            paths.Add("/category");
+            paths.Add("/categories");
+            paths.Add("/comments");
+            paths.Add("/contacts");
+            paths.Add("/member");
+            paths.Add("/members");
+            paths.Add("/news");
+            paths.Add("/organizations");
+            paths.Add("/slide");
+            paths.Add("/slides");
+            paths.Add("/testimonials");
+            paths.Add("/user");
+            if (methods.Contains(method.ToLower()) && paths.Contains(path.ToLower()))
+            {
+                if (!context.User.IsInRole("Administrador"))
+                {
+                    context.Response.StatusCode = 403;
+                    return;
+                }
+            }
+            await _next.Invoke(context);
+
+        }
+    }
+}
