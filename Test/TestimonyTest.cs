dd27a59e1ce4319c077f5fc54f7bd373dd8a0901
Pablo <pluna.e@hotmail.com>
2022-08-17 10:28:49 -0300

    added TestimonyTest

---
+++ Test/TestimonyTest.cs
@@ -0,0 +1,68 @@
+using System;
+using Xunit;
+
+namespace Test
+{
+    public class TestimonyTest
+    {
+        private readonly ITestimonialsService _context = A.Fake<ITestimonialsService>();
+
+        [Fact]
+        public void Post_Testimony()
+        {
+
+            CreationTestimonyDTO testimony = new CreationTestimonyDTO()
+            {
+                //Name = "Test",
+                //FacebookUrl = "TestFacebook.com",
+                //InstagramUrl = "TestInstagram.com",
+                //LinkedinUrl = "TestLinkedin.com",
+                //Image = "TestImage",
+            };
+
+            var controler = new TestimonialsController(_context);
+            var result = controler.Post(testimony);
+            Xunit.Assert.NotNull(result);
+        }
+
+
+        //[Fact]
+        //public void Put_Testimony()
+        //{
+        //    TestClass member = new MemberDto()
+        //    {
+        //        Name = "Test",
+        //        FacebookUrl = "TestFacebook.com",
+        //        InstagramUrl = "TestInstagram.com",
+        //        LinkedinUrl = "TestLinkedin.com",
+        //        Image = "TestImage",
+        //    };
+
+        //    var controler = new MemberController(_context);
+        //    var result = controler.Put(member, 1);
+        //    Xunit.Assert.NotNull(result);
+        //}
+
+
+        [Fact]
+        public void Get_Test()
+        {
+            var controler = new TestimonialsController(_context);
+
+            var test = controler.Get(1);
+            Xunit.Assert.NotNull(test);
+        }
+
+
+
+        [Fact]
+        public void Delete_Test()
+        {
+            var controler = new TestimonialsController(_context);
+            var result = controler.Delete(1);
+            Xunit.Assert.NotNull(result);
+        }
+    }
+}
+
+
